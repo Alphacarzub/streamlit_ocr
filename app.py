e05637e9373a96c44709780de9b853c78589e4be
Sarang Kulkarni <60709787+Alphacarzub@users.noreply.github.com>
2021-12-18 19:30:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("Optical Character Recognition (OCR) :")
 st.text('Upload the image :')
 
 # To ask for a file from user and then open do the following:-
-uploaded_file = st.file_uploader("choose an image :",type=['jpg','png','jpeg']) # We can give all formats.
+uploaded_file = st.file_uploader("choose an image :",type=['jpg','png','jpeg'])  # We can give all formats.
 if uploaded_file is not None: # if there is some file uploaded, then do the following - 
     img  = Image.open(uploaded_file) # reads the file uploaded(similiar to cv2.imread)
 if st.button('Predict'):
